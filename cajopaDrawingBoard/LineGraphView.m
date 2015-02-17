06af542f4f452029bc29a08c0c44f02ad9e2550e
Carl Parker <Carl.Parker@hbo.com>
2015-02-16 21:33:07 -0800

    successfully draw a bezier path, but does not resize
    for some good info on bezier paths:
    
      https://developer.apple.com/library/mac/documentation/Cocoa
      /Conceptual/CocoaDrawingGuide/Paths/Paths.html
    
    (Freakin' too long URLs)

--- cajopaDrawingBoard/LineGraphView.m
+++ cajopaDrawingBoard/LineGraphView.m
@@ -17,6 +17,33 @@
     
     [[NSColor orangeColor] set];
     [NSBezierPath fillRect:dirtyRect];
+    
+    NSBezierPath * circlePath = [NSBezierPath bezierPath];
+ 
+//    NSPoint startPoint = NSMakePoint(100, 100);
+//    NSPoint endPoint = NSMakePoint(200, 200);
+//    [circlePath moveToPoint:startPoint];
+//    [circlePath appendBezierPathWithArcFromPoint:startPoint toPoint:endPoint radius:20];
+//
+//    [circlePath appendBezierPathWithArcFromPoint:endPoint toPoint:startPoint radius:300];    
+
+    [circlePath moveToPoint:NSMakePoint(0.0, 0.0)];
+    
+    [circlePath lineToPoint:NSMakePoint(100.0, 100.0)];
+    
+    [circlePath curveToPoint:NSMakePoint(180.0, 210.0)
+     
+          controlPoint1:NSMakePoint(60.0, 20.0)
+     
+          controlPoint2:NSMakePoint(280.0, 100.0)];
+
+    [circlePath closePath];
+    
+    [[NSColor blueColor] set];
+    
+    [circlePath stroke];
+    
+
 }
 
 @end
