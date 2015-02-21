c5af6d13c8e9f371372a28de607327f5e2dce285
Carl Parker <Carl.Parker@hbo.com>
2015-02-20 19:02:34 -0800

    graph is complete and beautiful

--- cajopaDrawingBoard/LineGraphView.m
+++ cajopaDrawingBoard/LineGraphView.m
@@ -36,11 +36,11 @@
         NSLog(@"Peak #%lu: %f", (unsigned long)idx, printPercent);
     }];
     
-    [[NSColor orangeColor] set];
+    [[NSColor yellowColor] set];
     [NSBezierPath fillRect:dirtyRect];
     
     [[NSColor blackColor] setStroke];
-    [[NSColor redColor] setFill];
+    [[NSColor orangeColor] setFill];
   
     
     float graphWidth = self.bounds.size.width;
@@ -49,25 +49,33 @@
     NSBezierPath * myGraphPath = [NSBezierPath bezierPath];
     
     NSPoint startPoint = NSMakePoint(0, 0.05 * graphHeight);
+    NSPoint startGraph = NSMakePoint(0.05 * graphWidth, 0.05 * graphHeight);
+    
+    NSPoint endGraph = NSMakePoint(0.95 * graphWidth, 0.05 * graphHeight);
     NSPoint endPoint = NSMakePoint(graphWidth, 0.05 * graphHeight);
+    
+    [myGraphPath moveToPoint:NSMakePoint(0,0)];
+    [myGraphPath lineToPoint:startPoint];
+    [myGraphPath lineToPoint:startGraph];
+    [myGraphPath stroke];
 
     
-    [myGraphPath moveToPoint:startPoint];
-    
-    float widthIncrement = graphWidth / self.peaks.count-1;
+    float widthIncrement = (graphWidth * 0.90) / self.peaks.count-1;
     float currentWidthPos = startPoint.x + widthIncrement;
-    
     
     for (id nextPeak in self.peaks ) {
         [myGraphPath lineToPoint:NSMakePoint(currentWidthPos, ((NSNumber *)nextPeak).floatValue * graphHeight)];
         [myGraphPath stroke];
                                              
         currentWidthPos = currentWidthPos + widthIncrement;
-
     }
     
+    [myGraphPath lineToPoint:endGraph];
     [myGraphPath lineToPoint:endPoint];
+    [myGraphPath lineToPoint:NSMakePoint(graphWidth, 0)];
+    [myGraphPath closePath];
     [myGraphPath stroke];
+    [myGraphPath fill];
     
 }
 
