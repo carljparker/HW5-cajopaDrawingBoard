d4034a246ad59518e942513958222b0a7a9436f6
Carl Parker <Carl.Parker@hbo.com>
2015-02-20 19:52:19 -0800

    shapes are done . . . did not branch for that

--- cajopaDrawingBoard/ShapesView.m
+++ cajopaDrawingBoard/ShapesView.m
@@ -24,6 +24,50 @@
     [[NSColor yellowColor] set];
     [NSBezierPath fillRect:dirtyRect];
     
+    [[NSColor blackColor] setStroke];
+    [[NSColor orangeColor] setFill];
+    
+    
+    float drawingWidth = self.bounds.size.width;
+    float drawingHeight = self.bounds.size.height;
+    
+    float shapeDimension = drawingWidth / 4;
+    float shapeSeparation = shapeDimension / 4;
+    
+    NSBezierPath * myDrawingPath = [NSBezierPath bezierPath];
+    
+   // Begin Square
+    
+    [myDrawingPath moveToPoint:NSMakePoint(shapeSeparation, shapeSeparation)];
+    
+    [myDrawingPath lineToPoint:NSMakePoint(shapeSeparation, shapeSeparation + shapeDimension)];
+    [myDrawingPath lineToPoint:NSMakePoint(shapeSeparation + shapeDimension, shapeSeparation + shapeDimension)];
+    [myDrawingPath lineToPoint:NSMakePoint(shapeSeparation + shapeDimension, shapeSeparation)];
+    [myDrawingPath closePath];
+    
+    [myDrawingPath stroke];
+    [myDrawingPath fill];
+    
+    // Begin Triangle
+    
+    [myDrawingPath moveToPoint:NSMakePoint( (shapeSeparation * 2 ) + shapeDimension, shapeSeparation)];
+    
+    [myDrawingPath lineToPoint:NSMakePoint( (shapeSeparation * 2 ) + (shapeDimension * 1.5), shapeSeparation + shapeDimension)];
+    [myDrawingPath lineToPoint:NSMakePoint( (shapeSeparation * 2 ) + (shapeDimension * 2), shapeSeparation)];
+    [myDrawingPath closePath];
+
+    [myDrawingPath stroke];
+    [myDrawingPath fill];
+   
+    // Begin Circle
+    
+    [myDrawingPath moveToPoint:NSMakePoint( (shapeSeparation * 3 ) + ( shapeDimension * 2.5 ), shapeSeparation + ( shapeDimension / 2 ))];
+    
+    [myDrawingPath appendBezierPathWithArcWithCenter:myDrawingPath.currentPoint radius:( shapeDimension / 2 ) startAngle:0 endAngle:360];
+    
+    [myDrawingPath stroke];
+    [myDrawingPath fill];    
+    
 }
 
 @end
